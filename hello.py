2c2c677ef3c0791dcd6dc36c5f84de1e00a24a91
indra.harahap <indra_hrp@yahoo.com>
2018-02-18 16:55:36 -0500

    secon

---
+++ hello.py
@@ -0,0 +1,18 @@
+import os
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'Users %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 5000))))
